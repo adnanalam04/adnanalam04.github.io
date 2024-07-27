b96701e9dbae69acd66442b4a810b140f523c468
adnanalam04 <2022000344.mdadnan@ug.sharda.ac.in>
2024-07-28 01:49:08 +0530

    Deployment 1.0

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import { motion } from "framer-motion"
 import { FaGraduationCap, FaCertificate, FaTools } from "react-icons/fa"
 import "../styles/about.css"
